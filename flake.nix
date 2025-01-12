5e2ca775144183a6fa3388768b35ff1b3a3add39
Tylor Bradbury <tbradbury@me.com>
2025-01-12 15:52:04 -0700

    change host name inspiron-5368 to laptop

--- flake.nix
+++ flake.nix
@@ -17,10 +17,10 @@
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       # TODO please change the hostname to your own
-      inspiron-5368 = nixpkgs.lib.nixosSystem {
+      laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./inspiron-5368/configuration.nix
+          ./laptop/configuration.nix
 
           # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
@@ -30,7 +30,7 @@
             home-manager.useUserPackages = true;
 
             # TODO replace ryan with your own username
-            home-manager.users.tylor = import ./inspiron-5368/home.nix;
+            home-manager.users.tylor = import ./laptop/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "inspiron-5368"; # Define your hostname.
+  networking.hostName = "laptop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
