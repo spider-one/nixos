4fdbe134c48eb6840b7ac1b8c4deeb0354822156
Tylor Bradbury <tbradbury@me.com>
2025-01-12 15:27:06 -0700

    home-manager

--- flake.nix
+++ flake.nix
@@ -4,9 +4,18 @@
   inputs = {
     # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }@inputs: {
     # Please replace nixosConfigurations.my-nixos with your hostname
     nixosConfigurations.inspiron-5368 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -14,6 +23,18 @@
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./inspiron-5368/configuration.nix
+
+        # make home-manager as a module of nixos
+        # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          # TODO replace ryan with your own username
+          home-manager.users.tylor = import ./inspiron-5368/home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
       ];
     };
   };
