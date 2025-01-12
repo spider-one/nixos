3ed093efa668c19d03473946575ff4be93444051
Tylor Bradbury <tbradbury@me.com>
2025-01-12 15:31:29 -0700

    home-manager

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
             home-manager.useUserPackages = true;
 
             # TODO replace ryan with your own username
-            home-manager.users.tylor = import ./nspiron-5368/home.nix;
+            home-manager.users.tylor = import ./inspiron-5368/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
