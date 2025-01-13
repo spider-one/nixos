9c58b0d2efc1d879a641785d13e7ac957dd92022
Tylor Bradbury <tbradbury@me.com>
2025-01-12 17:31:22 -0700

    updates

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
